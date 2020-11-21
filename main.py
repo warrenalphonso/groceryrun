c2191a9cde24a620c4b87f7207b5debcfd61d598
Warren Alphonso <warrenalphonso02@gmail.com>
2020-11-20 19:18:41 -0800

    Loaded background and resized

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 20 17:42:02 2020
+import pyglet
+# Set resource path to assets/ 
+pyglet.resource.path = ['assets']
+pyglet.resource.reindex()
 
-@author: harveersingh
-"""
+# home_background.png is 2457 x 1397
+width = 2457 // 2
+height = 1397 // 2
+window = pyglet.window.Window(width, height)
+home_background = pyglet.resource.image("home_background.png")
+home_background.width = width 
+home_background.height = height
 
-print("lets gooo")
+@window.event 
+def on_draw():
+    window.clear()
+    home_background.blit(0,0)
+
+pyglet.app.run()
--- untitled0.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 20 17:41:42 2020
-
-@author: harveersingh
-"""
-
