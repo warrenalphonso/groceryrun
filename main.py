9cd52c6de49f754def859cecf2cfbe488d2b35cb
harveer737 <harveersingh737@gmail.com>
2020-11-20 17:43:28 -0800

    g

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 20 17:42:02 2020
+
+@author: harveersingh
+"""
+
+print("lets gooo")
---
+++ untitled0.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 20 17:41:42 2020
+
+@author: harveersingh
+"""
+
